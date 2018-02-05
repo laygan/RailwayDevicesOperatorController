d77434dc4859d7b51db93f23c6277aed0640def3
れいがん <its_me@laygan.net>
2018-02-05 22:38:19 +0000

    RaDO_Controller.ino edited online with Bitbucket

--- RaDO_Controller.ino
+++ RaDO_Controller.ino
@@ -1,70 +1,166 @@
-#define MAX_LENGTH 10
-#define MCP23S17_SSPIN 9
+/* MCP23S17 Pin Assign
+ * 
+ * B0 (0x80) A7 <-> Ding
+ * B1 (0x40) A6
+ * B2 (0x20) A5
+ * B3 (0x10) A4 <-> ATS-P Fail
+ * B4 (0x08) A3 <-> ATS-P Active
+ * B5 (0x04) A2 <-> ATS-P Break
+ * B6 (0x02) A1 <-> ATS-P Pattern
+ * B7 (0x01) A0 <-> ATS-P Power
+ * 
+ */
 
-#define ERROR_LAMP 6
-#define BELL 8
-#define BZ21 12
+#define MAX_LENGTH 10
+
+#define BELL 7
+#define BZ21 13
+
+// SPI IF
+#define DATAOUT 11//MOSI
+#define DATAIN  12//MISO 
+#define SPICLOCK  13//sck
+#define SLAVESELECT 10//ss
+
+// Interrupt
+#define INTPIN 2
+
+// MCP23S17 Device OPCODE
+#define HWADDW 0x40
+#define HWADDR 0x41
 
 #include <Metro.h>
-#include <Spi.h>
-#include <Mcp23s17.h>     // https://github.com/dreamcat4/Mcp23s17
-
-MCP23S17 ioic_0 = MCP23S17( MCP23S17_SSPIN, 0x0 );
-/**************************************************************************************
- *                    __________  ____________ 
- *   ATS-Sx Power ( 9)  1     B0     A7     28  ( 8) (C:10uF + R:4k)-> ding
- *  ATS-SX Active (10)  2     B1     A6     27  ( 7) 
- *    ATS-Sx Bell (11)  3     B2     A5     26  ( 6) ATS-P Fail
- * ATS-Sx BZ21Cut (12)  4  i  B3     A4  i  25  ( 5) ATS-P Active
- *                (13)  5  /  B4     A3  /  24  ( 4) ATS-P Purge
- *                (14)  6  o  B5     A2  o  23  ( 3) ATS-P Braking
- *                (15)  7     B6     A1     22  ( 2) ATS-P Pattern
- *                (16)  8 ___ B7     A0 ___ 21  ( 1) ATS-P Power
- *                      9     V+   intA     20  
- *                     10     GND  intB     19  
- *                     11     SS    RST     18  
- *                     12     CLK    A2     17  
- *                     13     SI     A1     16  
- *                     14     SO     A0     15  
- *                     ￣￣￣￣￣￣￣￣￣￣￣￣￣￣ 
- **************************************************************************************/
-
-const int pinMatrix[] = 
-  {
-    ERROR_LAMP,    // arduinoPin 0
-    ERROR_LAMP,    // arduinoPin 1
-    1,             // arduinoPin 2
-    2,             // arduinoPin 3
-    3,             // arduinoPin 4
-    4,             // arduinoPin 5
-    5,             // arduinoPin 6
-    6,             // arduinoPin 7
-    8,             // arduinoPin 8
-    9,             // arduinoPin 9
-    10,            // arduinoPin 10
-    ERROR_LAMP,    // arduinoPin 11
-    11,            // arduinoPin 12
-    12             // arduinoPin 13
-  };
-
-Metro ding = Metro(25);
+Metro ding = Metro(40);
 Metro bz21 = Metro(250);
 
-void setup() {
-  // MCP23S17 init
-  ioic_0.pinMode(OUTPUT);
+volatile byte address = 0;
+volatile byte data = 0;
+volatile byte deviceState = 0x00;
 
-  // all port pulldown to LOW
-  ioic_0.port(0x00);
+//SPI 関数
+byte spi_transfer(volatile byte data)
+{
+  SPDR = data;                    // Start the transmission
 
-  ioic_0.digitalWrite(11, HIGH);  // DC100V系統は負論理制御のため
+  while (!(SPSR & (1 << SPIF)))   // Wait the end of the transmission
+  {
+  };
 
+  return SPDR;                    // return the received byte
+}
+
+void initMCP23S17()
+{
+  //--------------------------------------------------------------
+  //pinmode 設定
+  
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
+  pinMode(SPICLOCK, OUTPUT);
+  pinMode(SLAVESELECT, OUTPUT);
+  digitalWrite(SLAVESELECT, HIGH); //disable device
+
+  //--------------------------------------------------------------
+  //SPI設定
+  
+  // SPCR = 01010000
+  //interrupt disabled,spi enabled,msb 1st,master,clk low when idle,
+  //sample on rising edge of clk,system clock/2 rate (fastest)
+  SPCR = 0x50;
+  SPSR = 0x01; // SPI2X:1
+
+  //--------------------------------------------------------------
+  //MCP23S17 初期化
+  
+  // IOCON(0x0A):0x20
+  //  BANK  :0
+  //  MIRROR:0
+  //  SEQOP :1
+  //  DISSLW:0
+  //  HAEN  :0
+  //  ODR   :0
+  //  INTPOL:0
+  //  -     :0
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x0A;
+  spi_transfer(address);   //send MSByte address first
+  data = 0x20;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+
+  // GPA -----------------------------------------------
+  // Set as outputs
+  // IODIRA(0x00): 1:input 0:output
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x00;
+  spi_transfer(address);   //send MSByte address first
+  data = 0x00;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+
+  // GPB -----------------------------------------------
+  // Set as outputs
+  // IODIRB(0x01): 1:input 0:output
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x01;
+  spi_transfer(address);   //send MSByte address first
+  data = 0x00;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+/*
+  // Pull-up
+  // GPPUB(0x0D): 1:pull-up enable 0:pull-up disable
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x0D;
+  spi_transfer(address);   //send MSByte address first
+  data = 0xFF;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+  
+  // 入力ピンの論理値を反転
+  // IPOLB(0x03): 1:opposite 0:same 
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x03;
+  spi_transfer(address);   //send MSByte address first
+  data = 0xFF;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+  
+  // Interrupt-on-change enable
+  // GPINTENB(0x05): 1:enable 0:disable
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);   //send MSByte address first
+  address = 0x05;
+  spi_transfer(address);   //send MSByte address first
+  data = 0xFF;
+  spi_transfer(data);      //send MSByte DATA
+  digitalWrite(SLAVESELECT, HIGH); //release chip
+  
+  //--------------------------------------------------------------
+  //外部割込み
+  pinMode(INTPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTPIN), dataChanged, FALLING);
+*/
+}
+
+
+int deviceMap[9] = {0, 0, 1, 2, 3, 4, 5, 6, 7};
+
+void setup()
+{
+  initMCP23S17();
+  
   // シリアル通信準備
   Serial.begin(115200);
 
   // プラグインとのハンドシェイク
   while (1) {
-    ioic_0.digitalWrite(ERROR_LAMP, HIGH);
+    driveDevice(6, true);
 
     Serial.println("1");
     if (Serial.available())
@@ -73,23 +169,42 @@ void setup() {
       if (temp == '0') { break; }
     }
     delay(500);
-    ioic_0.digitalWrite(ERROR_LAMP, LOW);
+    driveDevice(6, false);
     delay(500);
   }
   
   while (Serial.read() != -1) ;
-  ioic_0.digitalWrite(ERROR_LAMP, LOW);
+  driveDevice(6, false);
   
   Serial.println("1");
 }
 
+void driveDevice(int dev, bool value)
+{
+  digitalWrite(SLAVESELECT, LOW);
+  spi_transfer(HWADDW);
+  spi_transfer(0x12);
+  if(value) {
+    deviceState = deviceState | 1<<dev;
+  } else {
+    deviceState = deviceState & ~(1<<dev);
+  }
+  spi_transfer(deviceState);
+  digitalWrite(SLAVESELECT, HIGH);
+}
+
+bool ding_state = false;
+bool bz21_state = false;
+
 void loop()
 {
   // 単打ベル電源カットタイマー
-  if (ding.check() == 1) { flash_down(BELL); }
+  if (ding_state){
+    if (ding.check() == 1) put_signal(BELL, false);
+  }
 
   // BZ21警報器回路復帰タイマー
-  if (bz21.check() == 1) { flash_down(BZ21); }
+//  if (bz21.check() == 1) { flash_down(BZ21); }
 
   // 制御信号解析
   if (Serial.available()) { parse_signal(); }
@@ -108,61 +223,29 @@ void parse_signal()
   } while (str[p-1] != '\n');
   str[p-1] = '\0';
 
-  const int index = atoi(strtok(str, "," ));
+  const int device = atoi(strtok(str, "," ));
   const int value = atoi(strtok(NULL, "," ));
 
   // 機器操作
-  put_signal(pinMatrix[index], value);
-
-  // アンサーバック
-  Serial.println(String(index)+","+String(value));
+  put_signal(deviceMap[device], value);
 }
 
-void put_signal(int pin, int sig)
+void put_signal(int device, int value)
 {
-  /********* ATS-Sx 警報ベル制御 *********/
-  if (pin == 12)
-  {
-    if (sig == 0)
-    {
-      ioic_0.digitalWrite(pin, HIGH);       // ATS-Sx 警報ベル鳴動
-      flash_down(BZ21);              // ATS-Sx BZ21警報器回路開通
-    }
-    else { ioic_0.digitalWrite(pin, LOW); } // ATS-Sx 警報ベル停止
+  switch(device) {
+    case BELL:
+      if(!value) {
+        ding_state = false;
+      }
+      else {
+        ding_state = true;
+        ding.reset();
+      }
+      driveDevice(device, ding_state);
+      break;
+
+    default:
+      if (value == 0) driveDevice(device, false);
+      else driveDevice(device, true);
   }
-
-  /** ATC/S-P 単打ベル,表示器 BZ21警報器回路 制御 **/
-  else if (pin>=2 && pin<=13)
-  {
-    // フラッシュ制御機器動作(単打ベル,BZ21)
-    if (pin == BELL | pin == BZ21) { flash_up(pin); }
-
-    // その他機器制御
-    else
-    {
-      if (sig == 0) { ioic_0.digitalWrite(pin, LOW); }
-      else { ioic_0.digitalWrite(pin, HIGH); }
-    }
-  }
-
-  // 解析失敗
-  else
-  {
-    Serial.println("Opps!");
-    ioic_0.digitalWrite(ERROR_LAMP, HIGH);
-  }
-  
-}
-
-void flash_up(int pin)
-{
-  ioic_0.digitalWrite(pin, LOW);             // 継続動作防止
-  ioic_0.digitalWrite(pin, HIGH);
-  if (pin == BELL) { ding.reset(); }  // 電源カットタイマーリセット
-  else { bz21.reset(); }              // 回路復帰タイマーリセット
-}
-
-void flash_down(int pin)
-{
-  ioic_0.digitalWrite(pin, LOW);
 }
